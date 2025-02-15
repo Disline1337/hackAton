b39748221a3c2e38ceb54d0b9b936ed79314da06
1Sente <kokorin2006senya@yandex.ru>
2025-02-15 13:36:24 +0500

    elk

--- pages/index.js
+++ pages/index.js
@@ -4,12 +4,18 @@ export default function Home() {
     return (
         <div style={{ padding: '20px', fontFamily: 'Arial, sans-serif' }}>
             <h1>Главная страница</h1>
-            <p>
-                Перейти на страницу входа:{' '}
-                <Link href="/login">
-                    <a style={{ color: 'blue', textDecoration: 'underline' }}>Войти</a>
-                </Link>
-            </p>
+
+            <Link
+                href="/login"
+                style={{
+                    color: 'blue',
+                    textDecoration: 'underline',
+                    display: 'block',
+                    marginTop: '20px'
+                }}
+            >
+                Войти
+            </Link>
         </div>
     );
 }
--- pages/login.js
+++ pages/login.js
@@ -4,15 +4,21 @@ export default function LoginPage() {
     return (
         <div style={{ padding: '20px', fontFamily: 'Arial, sans-serif' }}>
             <h1>Страница входа</h1>
-            <p>
-                <Link href="/api/auth/elk">
-                    <a style={{ color: 'blue', textDecoration: 'underline' }}>Войти через ЕЛК</a>
-                </Link>
-            </p>
-            <p>
-                Вернуться на главную страницу:{' '}
-                <Link href="/">
-                    <a style={{ color: 'blue', textDecoration: 'underline' }}>Главная</a>
+
+            {/* Исправленная ссылка */}
+            <Link
+                href="/api/auth/elk"
+                style={{ color: 'blue', textDecoration: 'underline' }}
+            >
+                Войти через ЕЛК
+            </Link>
+
+            <p style={{ marginTop: '20px' }}>
+                <Link
+                    href="/"
+                    style={{ color: 'blue', textDecoration: 'underline' }}
+                >
+                    Главная
                 </Link>
             </p>
         </div>
