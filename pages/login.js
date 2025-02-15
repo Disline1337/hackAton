04c22d34beca5dbd533f7ca7030f22d3c773fdfc
1Sente <kokorin2006senya@yandex.ru>
2025-02-15 14:52:17 +0500

    elk

--- pages/login.js
+++ pages/login.js
@@ -1,25 +1,15 @@
-import { useRouter } from 'next/router';
 import Link from 'next/link';
 
 export default function LoginPage() {
-    const router = useRouter();
-    const { error } = router.query;
-
     return (
         <div style={{ padding: '20px', fontFamily: 'Arial, sans-serif' }}>
             <h1>Страница входа</h1>
-            {error && <p style={{ color: 'red' }}>Ошибка: {error}</p>}
             <p>
                 <Link
-                    href="/elk_callback"
+                    href={`https://lk.orb.ru/oauth/authorize?client_id=25&redirect_uri=http://hackathon_10.orb.ru/elk_callback&response_type=code&scope=personal_data+email+phone&state=http://hackathon_10.orb.ru/`}
                     style={{ color: 'blue', textDecoration: 'underline' }}
                 >
                     Войти через ЕЛК
-                </Link>
-            </p>
-            <p style={{ marginTop: '20px' }}>
-                <Link href="/" style={{ color: 'blue', textDecoration: 'underline' }}>
-                    Вернуться на главную
                 </Link>
             </p>
         </div>
