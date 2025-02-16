5e0b766c8ab5e72c6c3a5e9443c3ee8568046f1b
Кирилл.Л <133775527+voxigen@users.noreply.github.com>
2025-02-16 13:18:07 +0500

    Com5

--- components/HumanBlock/HumanBlock.js
+++ components/HumanBlock/HumanBlock.js
@@ -1,24 +1,42 @@
-import style from "./HumanBlock.module.css"
-export default function HumanBlock({props}) {
+
+import style from "./HumanBlock.module.css";
+
+export default function HumanBlock({ props, onClick }) {
+    const handleClick = () => {
+        if (onClick) {
+            onClick(props); 
+        }
+    };
+
     return (
-        <div className={style.HumanBlock}>
-            <div className={style.imageContainer}>
-                <img
-                    src={props.image}
-                    alt={props.name}
-                    className={style.image}
-                />
-            </div>
+        <div className={style.HumanBlock} onClick={handleClick}>
+            {props.image && (
+                <div className={style.imageContainer}>
+                    <img
+                        src={props.image}
+                        alt={props.name}
+                        className={style.image}
+                    />
+                </div>
+            )}
 
             <div className={style.content}>
                 <h3 className={style.name}>{props.name}</h3>
-                <p className={style.descript}>{props.descript}</p>
-                <div className={style.bornDate}>
-                     {props.born}
+
+                <div className={style.infoRow}>
+                    <div className={style.infoItem}>
+                        <h4 className={style.infoTitle}>Basic Information</h4>
+                        <p className={style.text}>{props.description}</p>
+                    </div>
+
+                    <div className={style.infoItem}>
+                        <h4 className={style.infoTitle}>Date and Place of Birth</h4>
+                        <p className={style.text}>
+                            {props.DateAndPlaceOfBirth?.join(', ')}
+                        </p>
+                    </div>
                 </div>
             </div>
         </div>
-    )
+    );
 }
-
-
